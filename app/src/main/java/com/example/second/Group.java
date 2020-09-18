759620ec3c78904fdd8edf684648d98c08f6171e
lgheric <saverneo@163.com>
2020-09-19 07:01:13 +0800

    The base useage of ExpandableListView

---
+++ app/src/main/java/com/example/second/Group.java
@@ -0,0 +1,21 @@
+package com.example.second;
+
+public class Group {
+    private String gName;
+
+    public Group() {
+    }
+
+    public Group(String gName) {
+        this.gName = gName;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+}
+
--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,33 +1,75 @@
 package com.example.second;
 
 
+import android.content.Context;
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.MultiAutoCompleteTextView;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String[] data = new String[]{
-            "X小猪猪", "X小狗狗", "X小鸡鸡", "X小猫猫", "X小咪咪"
-    };
+    private ArrayList<ArrayList<Item>> iData = null;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AutoCompleteTextView atv_content = findViewById(R.id.atv_content);
-        MultiAutoCompleteTextView matv_content = findViewById(R.id.matv_content);
+        mContext = MainActivity.this;
+        ExpandableListView exlist_lol = findViewById(R.id.exlist_lol);
 
+        //数据准备
+        ArrayList<Group> gData = new ArrayList<>();
+        iData = new ArrayList<>();
+        gData.add(new Group("AD"));
+        gData.add(new Group("AP"));
+        gData.add(new Group("TANK"));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, data);
-        atv_content.setAdapter(adapter);
+        ArrayList<Item> lData = new ArrayList<>();
 
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line, data);
-        matv_content.setAdapter(adapter2);
-        matv_content.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        //AD组
+        lData.add(new Item(R.mipmap.iv_lol_icon3,"剑圣"));
+        lData.add(new Item(R.mipmap.iv_lol_icon4,"德莱文"));
+        lData.add(new Item(R.mipmap.iv_lol_icon13,"男枪"));
+        lData.add(new Item(R.mipmap.iv_lol_icon14,"韦鲁斯"));
+        iData.add(lData);
+
+        //AP组
+        lData = new ArrayList<>();
+        lData.add(new Item(R.mipmap.iv_lol_icon1, "提莫"));
+        lData.add(new Item(R.mipmap.iv_lol_icon7, "安妮"));
+        lData.add(new Item(R.mipmap.iv_lol_icon8, "天使"));
+        lData.add(new Item(R.mipmap.iv_lol_icon9, "泽拉斯"));
+        lData.add(new Item(R.mipmap.iv_lol_icon11, "狐狸"));
+        iData.add(lData);
+
+        //TANK组
+        lData = new ArrayList<>();
+        lData.add(new Item(R.mipmap.iv_lol_icon2, "诺手"));
+        lData.add(new Item(R.mipmap.iv_lol_icon5, "德邦"));
+        lData.add(new Item(R.mipmap.iv_lol_icon6, "奥拉夫"));
+        lData.add(new Item(R.mipmap.iv_lol_icon10, "龙女"));
+        lData.add(new Item(R.mipmap.iv_lol_icon12, "狗熊"));
+        iData.add(lData);
+
+        MyBaseExpandableListAdapter myAdapter = new MyBaseExpandableListAdapter(gData, iData, mContext);
+        exlist_lol.setAdapter(myAdapter);
+
+        //为列表设置点击事件
+        exlist_lol.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Toast.makeText(mContext, "你点击了：" + iData.get(groupPosition).get(childPosition).getiName(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
     }
 
 
