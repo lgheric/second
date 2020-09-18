8ea954c4640224161bd19efc27b77ec8f706744e
lgheric <saverneo@163.com>
2020-09-18 19:10:48 +0800

    AutoCompleteTextView and Multi AutocompleteTextView completes

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,82 +1,35 @@
 package com.example.second;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    private Context mContext;
-    //判断是否为刚进去时触发onItemSelected的标志
-    private boolean one_selected = false;
-    private boolean two_selected = false;
-    private ArrayList<Hero> mData = null;
+    private static final String[] data = new String[]{
+            "X小猪猪", "X小狗狗", "X小鸡鸡", "X小猫猫", "X小咪咪"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContext = MainActivity.this;
-        mData = new ArrayList<>();
-        bindViews();
+        AutoCompleteTextView atv_content = findViewById(R.id.atv_content);
+        MultiAutoCompleteTextView matv_content = findViewById(R.id.matv_content);
 
-    }
 
-    private void bindViews() {
-        Spinner spin_one = findViewById(R.id.spin_one);
-        Spinner spin_two = findViewById(R.id.spin_two);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, data);
+        atv_content.setAdapter(adapter);
 
-        mData.add(new Hero(R.mipmap.iv_lol_icon1,"迅捷斥候：提莫（Teemo）"));
-        mData.add(new Hero(R.mipmap.iv_lol_icon2,"诺克萨斯之手：德莱厄斯（Darius）"));
-        mData.add(new Hero(R.mipmap.iv_lol_icon3,"无极剑圣：易（Yi）"));
-        mData.add(new Hero(R.mipmap.iv_lol_icon4,"德莱厄斯：德莱文（Draven）"));
-        mData.add(new Hero(R.mipmap.iv_lol_icon5,"德邦总管：赵信（XinZhao）"));
-        mData.add(new Hero(R.mipmap.iv_lol_icon6,"狂战士：奥拉夫（Olaf）"));
-
-        BaseAdapter myAdadpter = new MyAdapter<Hero>(mData, R.layout.item_spin_hero) {
-            @Override
-            public void bindView(ViewHolder holder, Hero obj) {
-                holder.setImageResource(R.id.img_icon, obj.gethIcon());
-                holder.setText(R.id.txt_name, obj.gethName());
-            }
-        };
-        spin_two.setAdapter(myAdadpter);
-
-        spin_one.setOnItemSelectedListener(this);
-        spin_two.setOnItemSelectedListener(this);
-
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line, data);
+        matv_content.setAdapter(adapter2);
+        matv_content.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
     }
 
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch (parent.getId()){
-            case R.id.spin_one:
-                if(one_selected){
-                    Toast.makeText(mContext,"您的分段是~：" + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
-                }else one_selected = true;
-                break;
-            case R.id.spin_two:
-                if(two_selected){
-                    TextView txt_name = view.findViewById(R.id.txt_name);
-                    Toast.makeText(mContext,"您选择的英雄是~：" + txt_name.getText().toString(), Toast.LENGTH_SHORT).show();
-                }else two_selected = true;
-                break;
-        }
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
