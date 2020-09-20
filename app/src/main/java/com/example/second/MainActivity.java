8000e1cb6d516a6d55299bf2c2f0c0d095d79c7f
lgheric <saverneo@163.com>
2020-09-20 18:01:26 +0800

    the base usage of PopupWindow

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,67 +1,84 @@
 package com.example.second;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TimePicker;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
-import java.util.Calendar;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    private String result = "";
+    private Context mContext;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindViews();
+
+
+        mContext = MainActivity.this;
+        Button btn_show = findViewById(R.id.btn_show);
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initPopWindow(v);
+            }
+        });
     }
 
-    private void bindViews() {
-        Button btn_date = findViewById(R.id.btn_date);
-        Button btn_time = findViewById(R.id.btn_time);
+    private void initPopWindow(View v) {
+        @SuppressLint("InflateParams") View view = LayoutInflater.from(mContext).inflate(R.layout.item_popup, null, false);
+        Button btn_xixi = view.findViewById(R.id.btn_xixi);
+        Button btn_hehe = view.findViewById(R.id.btn_hehe);
+        //1.构造一个PopupWindow，参数依次是加载的View，宽高
+        final PopupWindow popWindow = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
 
-        btn_date.setOnClickListener(this);
-        btn_time.setOnClickListener(this);
+        popWindow.setAnimationStyle(R.anim.anim_pop);  //设置加载动画
+
+        //这些为了点击非PopupWindow区域，PopupWindow会消失的，如果没有下面的
+        //代码的话，你会发现，当你把PopupWindow显示出来了，无论你按多少次后退键
+        //PopupWindow并不会关闭，而且退不出程序，加上下述代码可以解决这个问题
+        popWindow.setTouchable(true);
+        popWindow.setTouchInterceptor(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+                // 这里如果返回true的话，touch事件将被拦截
+                // 拦截后 PopupWindow的onTouchEvent不被调用，这样点击外部区域无法dismiss
+            }
+        });
+        popWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));    //要为popWindow设置一个背景才有效
+
+
+        //设置popupWindow显示的位置，参数依次是参照View，x轴的偏移量，y轴的偏移量
+        popWindow.showAsDropDown(v, 50, 0);
+
+        //设置popupWindow里的按钮的事件
+        btn_xixi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "你点击了嘻嘻~", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_hehe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "你点击了呵呵~", Toast.LENGTH_SHORT).show();
+                popWindow.dismiss();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        result = "";
-        switch (v.getId()){
-            case R.id.btn_date:
-                Calendar cale1 = Calendar.getInstance();
-                new DatePickerDialog(MainActivity.this,new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        //这里获取到的月份需要加上1哦~
-                        result += "你选择的是"+year+"年"+(monthOfYear+1)+"月"+dayOfMonth+"日";
-                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                    }
-                }
-                        ,cale1.get(Calendar.YEAR)
-                        ,cale1.get(Calendar.MONTH)
-                        ,cale1.get(Calendar.DAY_OF_MONTH)).show();
-                break;
-            case R.id.btn_time:
-                Calendar cale2 = Calendar.getInstance();
-                new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        result = "";
-                        result += "您选择的时间是:"+hourOfDay+"时"+minute+"分";
-                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                    }
-                }, cale2.get(Calendar.HOUR_OF_DAY), cale2.get(Calendar.MINUTE), true).show();
-                break;
-        }
-    }
+
 }
