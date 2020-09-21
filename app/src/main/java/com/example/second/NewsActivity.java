ba1256e18db18e21be93db77fc2846088c745f99
lgheric <saverneo@163.com>
2020-09-21 09:16:07 +0800

    "android.support.*" have been migrated to "Androidx.appcompat.*"

--- app/src/main/java/com/example/second/NewsActivity.java
+++ app/src/main/java/com/example/second/NewsActivity.java
@@ -1,7 +1,7 @@
 package com.example.second;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NewsActivity extends AppCompatActivity {
     @Override
