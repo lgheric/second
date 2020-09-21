c9fd6a7dc613cdae45e5cb3d7f31647bb452f48b
lgheric <saverneo@163.com>
2020-09-21 17:03:20 +0800

    the base usage of PagerAdapter

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,152 +1,48 @@
 package com.example.second;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
-    //1.定义不同颜色的菜单项的标识:
-    final private int RED = 110;
-    final private int GREEN = 111;
-    final private int BLUE = 112;
-    final private int YELLOW = 113;
-    final private int GRAY= 114;
-    final private int CYAN= 115;
-    final private int BLACK= 116;
-
-    private TextView tv_test;
-    private TextView tv_context;
-    private Button btn_show_menu;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
-        tv_test = (TextView) findViewById(R.id.tv_test);
-        tv_context = (TextView) findViewById(R.id.tv_context);
-        registerForContextMenu(tv_context);
-        btn_show_menu = (Button) findViewById(R.id.btn_show_menu);
-        btn_show_menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(MainActivity.this,btn_show_menu);
-                popup.getMenuInflater().inflate(R.menu.menu_pop, popup.getMenu());
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()){
-                            case R.id.lpig:
-                                Toast.makeText(MainActivity.this,"你点了小猪~", Toast.LENGTH_SHORT).show();
-                                break;
-                            case R.id.bpig:
-                                Toast.makeText(MainActivity.this,"你点了大猪~", Toast.LENGTH_SHORT).show();
-                                break;
-                        }
-                        return true;
-                    }
-                });
-                popup.show();
-            }
-        });
-
+    private void initViews() {
+        Button btn_one = (Button) findViewById(R.id.btn_one);
+        Button btn_two = (Button) findViewById(R.id.btn_two);
+        Button btn_three = (Button) findViewById(R.id.btn_three);
+        Button btn_four = (Button) findViewById(R.id.btn_four);
+        btn_one.setOnClickListener(this);
+        btn_two.setOnClickListener(this);
+        btn_three.setOnClickListener(this);
+        btn_four.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menu.add(1,RED,4,"红色");
-        menu.add(1,GREEN,2,"绿色");
-        menu.add(1,BLUE,3,"蓝色");
-        menu.add(1,YELLOW,1,"黄色");
-        menu.add(1,GRAY,5,"灰色");
-        menu.add(1,CYAN,6,"蓝绿色");
-        menu.add(1,BLACK,7,"黑色");
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch (id){
-            case RED:
-                tv_test.setTextColor(Color.RED);
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_one:
+                startActivity(new Intent(this, OneActivity.class));
                 break;
-            case GREEN:
-                tv_test.setTextColor(Color.GREEN);
+            case R.id.btn_two:
+                //startActivity(new Intent(this, TwoActivity.class));
                 break;
-            case BLUE:
-                tv_test.setTextColor(Color.BLUE);
+            case R.id.btn_three:
+                //startActivity(new Intent(this, ThreeActivity.class));
                 break;
-            case YELLOW:
-                tv_test.setTextColor(Color.YELLOW);
-                break;
-            case GRAY:
-                tv_test.setTextColor(Color.GRAY);
-                break;
-            case CYAN:
-                tv_test.setTextColor(Color.CYAN);
-                break;
-            case BLACK:
-                tv_test.setTextColor(Color.BLACK);
+            case R.id.btn_four:
+                //startActivity(new Intent(this, FourActivity.class));
                 break;
         }
-        return super.onOptionsItemSelected(item);
+
     }
-
-
-    /* 重写与 ContextMenu相关方法 */
-    @Override
-    //重写上下文菜单的创建方法
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        MenuInflater inflator = new MenuInflater(this);
-        inflator.inflate(R.menu.menu_context, menu);
-        super.onCreateContextMenu(menu, v, menuInfo);
-    }
-
-    //上下文菜单被点击是触发该方法
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.blue:
-                tv_context.setTextColor(Color.BLUE);
-                break;
-            case R.id.green:
-                tv_context.setTextColor(Color.GREEN);
-                break;
-            case R.id.red:
-                tv_context.setTextColor(Color.RED);
-                break;
-
-            case R.id.one:
-                Toast.makeText(MainActivity.this,"你点击了子菜单一",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.two:
-                item.setCheckable(true);
-                Toast.makeText(MainActivity.this,"你点击了子菜单二",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.three:
-                Toast.makeText(MainActivity.this,"你点击了子菜单三",Toast.LENGTH_SHORT).show();
-                item.setCheckable(true);
-                break;
-
-        }
-        return true;
-    }
-
-
 }
---
+++ app/src/main/java/com/example/second/OneActivity.java
@@ -0,0 +1,30 @@
+package com.example.second;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+
+public class OneActivity extends AppCompatActivity {
+
+    @SuppressLint("InflateParams")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+        ViewPager vpager_one = (ViewPager) findViewById(R.id.vpager_one);
+
+        ArrayList<View> aList = new ArrayList<>();
+        LayoutInflater li = getLayoutInflater();
+        aList.add(li.inflate(R.layout.view_one,null,false));
+        aList.add(li.inflate(R.layout.view_two,null,false));
+        aList.add(li.inflate(R.layout.view_three,null,false));
+        MyPagerAdapter mAdapter = new MyPagerAdapter(aList);
+        vpager_one.setAdapter(mAdapter);
+    }
+}
