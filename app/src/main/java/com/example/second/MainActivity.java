04455e17023fc1e499c68e70d3cf8a8a6c4e8276
lgheric <saverneo@163.com>
2020-09-23 09:36:49 +0800

    the base usage of handler in UI thread

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,86 +1,57 @@
 package com.example.second;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentManager;
+
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity{
 
-    private DrawerLayout drawer_layout;
-    private RightFragment fg_right_menu;
-    private LeftFragment fg_left_menu;
+    //定义切换的图片的数组id
+    int[] imgids = new int[]{
+            R.drawable.s_1, R.drawable.s_2,R.drawable.s_3,
+            R.drawable.s_4,R.drawable.s_5,R.drawable.s_6,
+            R.drawable.s_7,R.drawable.s_8
+    };
+    int imgstart = 0;
+    ImageView imgchange;
+
+    final Handler myHandler = new Handler(Objects.requireNonNull(Looper.myLooper()))
+    {
+        @Override
+        //重写handleMessage方法,根据msg中what的值判断是否执行后续操作
+        public void handleMessage(Message msg) {
+            if(msg.what == 0x123)
+            {
+                //Log.d("DEBUG",imgstart+" "+(imgstart%8));
+                imgchange.setImageResource(imgids[imgstart++ % 8]);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //获取碎片管理器对象
-        FragmentManager fManager = getSupportFragmentManager();
+        imgchange = (ImageView) findViewById(R.id.imgchange);
 
-        fg_right_menu = (RightFragment) fManager.findFragmentById(R.id.fg_right_menu);//通过碎片管理器获得左侧滑的碎片对象
-        fg_left_menu = (LeftFragment) fManager.findFragmentById(R.id.fg_left_menu);//通过碎片管理器获得右侧滑的碎片对象
-
-        initViews();
+        //使用定时器,每隔200毫秒让handler发送一个空信息
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                myHandler.sendEmptyMessage(0x123);
+            }
+        }, 0,200);
 
     }
 
-    @SuppressLint("WrongConstant")
-    private void initViews() {
-        //---获得主侧滑布局
-        drawer_layout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        //FrameLayout fly_content = (FrameLayout) findViewById(R.id.fly_content);
-
-        //----获得主布局中include的布局topbar
-        View topbar = findViewById(R.id.topbar);
-        //获得topbar布局中的btn_right控件,并给其添加点击事件监听
-        Button btn_right = (Button) topbar.findViewById(R.id.btn_right);
-        btn_right.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                drawer_layout.openDrawer(GravityCompat.END);//打开右侧滑菜单
-                drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, GravityCompat.END);    //解除锁定
-            }
-
-        });
-
-        //---设置右面的侧滑菜单只能通过编程来打开
-        drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, GravityCompat.END);
-        //给主侧滑布局添加事件监听
-        drawer_layout.addDrawerListener(new DrawerLayout.DrawerListener() {
-            @Override
-            public void onDrawerSlide(@NonNull View view, float v) {
-
-            }
-
-            @Override
-            public void onDrawerOpened(@NonNull View view) {
-
-            }
-
-            @Override
-            public void onDrawerClosed(@NonNull View view) {
-                drawer_layout.setDrawerLockMode(
-                        DrawerLayout.LOCK_MODE_LOCKED_CLOSED, GravityCompat.END);
-            }
-
-            @Override
-            public void onDrawerStateChanged(int i) {
-
-            }
-        });
-
-        //---业务需要的自定义函数，用来把drawer_layout传给碎片对象
-        fg_right_menu.setDrawerLayout(drawer_layout);
-        fg_left_menu.setDrawerLayout(drawer_layout);
-    }
 }
