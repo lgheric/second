28aee8306f2e7db6cc7c329ffee431d6432c4409
lgheric <saverneo@163.com>
2020-10-21 15:30:46 +0800

    show and hide for EditText

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -2,15 +2,43 @@ package com.example.second;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edit_pawd;
+    private Button btnChange;
+    private boolean flag = false;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_pawd = (EditText) findViewById(R.id.edit_pawd);
+        btnChange = (Button) findViewById(R.id.btnChange);
+        edit_pawd.setHorizontallyScrolling(true);    //设置EditText不换行
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag){
+                    edit_pawd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    flag = false;
+                    btnChange.setText("密码不可见");
+                }else{
+                    edit_pawd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    flag = true;
+                    btnChange.setText("密码可见");
+                }
+            }
+        });
     }
 
 }
