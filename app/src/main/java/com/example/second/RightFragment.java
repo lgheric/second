ed61990df8225c69201b91c3a39d42af9b77abd1
lgheric <saverneo@163.com>
2020-10-21 14:03:31 +0800

    in.xml

--- app/src/main/java/com/example/second/RightFragment.java
+++ app/src/main/java/com/example/second/RightFragment.java
@@ -33,18 +33,18 @@ public class RightFragment extends Fragment implements View.OnClickListener{
                 //实例化一个碎片对象
                 ContentFragment cFragment1 = new ContentFragment("1.点击了右侧菜单项一",R.color.blue);
                 //通过碎片管理器替换内容显示区的内容（这里只改了文字和背景颜色，内容显示区是在主布局中）
-                fManager.beginTransaction().replace(R.id.fly_content,cFragment1).commit();
+                //fManager.beginTransaction().replace(R.id.fly_content,cFragment1).commit();
                 //关闭右侧滑菜单
                 drawer_layout.closeDrawer(GravityCompat.END);
                 break;
             case R.id.btn_two:
                 ContentFragment cFragment2 = new ContentFragment("2.点击了右侧菜单项二",R.color.red);
-                fManager.beginTransaction().replace(R.id.fly_content,cFragment2).commit();
+                //fManager.beginTransaction().replace(R.id.fly_content,cFragment2).commit();
                 drawer_layout.closeDrawer(GravityCompat.END);
                 break;
             case R.id.btn_three:
                 ContentFragment cFragment3 = new ContentFragment("3.点击了右侧菜单项三",R.color.yellow);
-                fManager.beginTransaction().replace(R.id.fly_content,cFragment3).commit();
+                //fManager.beginTransaction().replace(R.id.fly_content,cFragment3).commit();
                 drawer_layout.closeDrawer(GravityCompat.END);
                 break;
         }
