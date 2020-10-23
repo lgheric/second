048903c33618e97a9b90287ad720f27e2076a846
lgheric <saverneo@163.com>
2020-10-23 17:11:20 +0800

    the usage of onConfigurationChanged

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.second;
 
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,30 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtResult = (TextView) findViewById(R.id.txtResult);
-        StringBuilder status = new StringBuilder();
-        //①获取系统的Configuration对象
-        Configuration cfg = getResources().getConfiguration();
-        //②想查什么查什么
-        status.append("densityDpi:").append(cfg.densityDpi).append("\n");
-        status.append("fontScale:").append(cfg.fontScale).append("\n");
-        status.append("hardKeyboardHidden:").append(cfg.hardKeyboardHidden).append("\n");
-        status.append("keyboard:").append(cfg.keyboard).append("\n");
-        status.append("keyboardHidden:").append(cfg.keyboardHidden).append("\n");
-        status.append("locale:").append(cfg.locale).append("\n");
-        status.append("mcc:").append(cfg.mcc).append("\n");
-        status.append("mnc:").append(cfg.mnc).append("\n");
-        status.append("navigation:").append(cfg.navigation).append("\n");
-        status.append("navigationHidden:").append(cfg.navigationHidden).append("\n");
-        status.append("orientation:").append(cfg.orientation).append("\n");
-        status.append("screenHeightDp:").append(cfg.screenHeightDp).append("\n");
-        status.append("screenWidthDp:").append(cfg.screenWidthDp).append("\n");
-        status.append("screenLayout:").append(cfg.screenLayout).append("\n");
-        status.append("smallestScreenWidthDp:").append(cfg.densityDpi).append("\n");
-        status.append("touchscreen:").append(cfg.densityDpi).append("\n");
-        status.append("uiMode:").append(cfg.densityDpi).append("\n");
-        txtResult.setTextSize(24.0f);
-        txtResult.setText(status.toString());
+        Button btn = (Button) findViewById(R.id.btncahange);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Configuration config = getResources().getConfiguration();
+                //如果是横屏的话切换成竖屏
+                if(config.orientation == Configuration.ORIENTATION_LANDSCAPE)
+                {
+                    MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                }
+                //如果竖屏的话切换成横屏
+                if(config.orientation == Configuration.ORIENTATION_PORTRAIT)
+                {
+                    MainActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        String screen = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE?"横屏":"竖屏";
+        Toast.makeText(MainActivity.this, "系统屏幕方向发生改变 \n 修改后的方向为" + screen, Toast.LENGTH_SHORT).show();
     }
 
 }
