3368f459e289671d1e4aa209f353104a3cdc510e
lgheric <saverneo@163.com>
2020-10-23 16:25:55 +0800

    all the method of Configuration

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,44 +1,42 @@
 package com.example.second;
 
-import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.text.method.HideReturnsTransformationMethod;
-import android.text.method.PasswordTransformationMethod;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText edit_pawd;
-    private Button btnChange;
-    private boolean flag = false;
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edit_pawd = (EditText) findViewById(R.id.edit_pawd);
-        btnChange = (Button) findViewById(R.id.btnChange);
-        edit_pawd.setHorizontallyScrolling(true);    //设置EditText不换行
-        btnChange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(flag){
-                    edit_pawd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                    flag = false;
-                    btnChange.setText("密码不可见");
-                }else{
-                    edit_pawd.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                    flag = true;
-                    btnChange.setText("密码可见");
-                }
-            }
-        });
+        TextView txtResult = (TextView) findViewById(R.id.txtResult);
+        StringBuilder status = new StringBuilder();
+        //①获取系统的Configuration对象
+        Configuration cfg = getResources().getConfiguration();
+        //②想查什么查什么
+        status.append("densityDpi:").append(cfg.densityDpi).append("\n");
+        status.append("fontScale:").append(cfg.fontScale).append("\n");
+        status.append("hardKeyboardHidden:").append(cfg.hardKeyboardHidden).append("\n");
+        status.append("keyboard:").append(cfg.keyboard).append("\n");
+        status.append("keyboardHidden:").append(cfg.keyboardHidden).append("\n");
+        status.append("locale:").append(cfg.locale).append("\n");
+        status.append("mcc:").append(cfg.mcc).append("\n");
+        status.append("mnc:").append(cfg.mnc).append("\n");
+        status.append("navigation:").append(cfg.navigation).append("\n");
+        status.append("navigationHidden:").append(cfg.navigationHidden).append("\n");
+        status.append("orientation:").append(cfg.orientation).append("\n");
+        status.append("screenHeightDp:").append(cfg.screenHeightDp).append("\n");
+        status.append("screenWidthDp:").append(cfg.screenWidthDp).append("\n");
+        status.append("screenLayout:").append(cfg.screenLayout).append("\n");
+        status.append("smallestScreenWidthDp:").append(cfg.densityDpi).append("\n");
+        status.append("touchscreen:").append(cfg.densityDpi).append("\n");
+        status.append("uiMode:").append(cfg.densityDpi).append("\n");
+        txtResult.setTextSize(24.0f);
+        txtResult.setText(status.toString());
     }
 
 }
