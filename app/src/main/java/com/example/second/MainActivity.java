f81feca55497fbbae5c85cdd03419ac1c10337a4
lgheric <saverneo@163.com>
2020-10-24 17:22:23 +0800

    recognize custom gesture

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,53 +1,64 @@
 package com.example.second;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.gesture.Gesture;
 import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
-import android.graphics.Bitmap;
+import android.gesture.Prediction;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GestureLibrary gestureLibrary;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mContext = MainActivity.this;
+        gestureLibrary = GestureLibraries.fromFile("/mygestures");
+        if (gestureLibrary.load()) {
+            Toast.makeText(mContext, "手势库加载成功", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(mContext, "手势库加载失败", Toast.LENGTH_SHORT).show();
+        }
+
         //获取手势编辑组件后，设置相关参数
-        GestureOverlayView gesture = (GestureOverlayView) findViewById(R.id.gesture);
+        GestureOverlayView gesture = findViewById(R.id.gesture);
         gesture.setGestureColor(Color.GREEN);
         gesture.setGestureStrokeWidth(5);
         gesture.addOnGesturePerformedListener(new GestureOverlayView.OnGesturePerformedListener() {
             @Override
             public void onGesturePerformed(GestureOverlayView gestureOverlayView, final Gesture gesture) {
-                View saveDialog = getLayoutInflater().inflate(R.layout.dialog_save,null,false);
-                ImageView img_show = saveDialog.findViewById(R.id.img_show);
-                final EditText edit_name = saveDialog.findViewById(R.id.edit_name);
-                Bitmap bitmap = gesture.toBitmap(128,128,10,0xffff0000);
-                img_show.setImageBitmap(bitmap);
-                new AlertDialog.Builder(MainActivity.this).setView(saveDialog)
-                        .setPositiveButton("保存",new DialogInterface.OnClickListener()
-                        {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                //获取文件对应的手势库
-                                GestureLibrary gestureLib = GestureLibraries.fromFile("/mygestures");
-                                gestureLib.addGesture(edit_name.getText().toString(),gesture);
-                                gestureLib.save();
-                            }
-                        }).setNegativeButton("取消", null).show();
+                //识别用户刚绘制的手势
+                ArrayList<Prediction> predictions = gestureLibrary.recognize(gesture);
+                ArrayList<String> result = new ArrayList<>();
+                //遍历所有找到的Prediction对象
+                for (Prediction pred : predictions) {
+                    if (pred.score > 2.0) {
+                        result.add("与手势【" + pred.name + "】相似度为" + pred.score);
+                    }
+                }
+                if (result.size() > 0) {
+                    ArrayAdapter<Object> adapter = new ArrayAdapter<>(mContext,
+                            android.R.layout.simple_dropdown_item_1line, result.toArray());
+                    new AlertDialog.Builder(mContext).setAdapter(adapter,null).setPositiveButton("确定",null).show();
+                }else{
+                    Toast.makeText(mContext,"无法找到匹配的手势！",Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
 
 }
