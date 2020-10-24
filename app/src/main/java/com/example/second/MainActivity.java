793fa7fa2073fd9348090a5e1a5679c8657d78a4
lgheric <saverneo@163.com>
2020-10-24 16:29:05 +0800

    GestureListener

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,54 +1,68 @@
 package com.example.second;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView txttitle;
-    private ProgressBar pgbar;
-    private MyAsyncTask myTask;
+    private GestureDetector mDetector;
+    private final static String TAG = "MyGesture";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txttitle = findViewById(R.id.txttitle);
-        pgbar = findViewById(R.id.pgbar);
-        Button btnupdate = findViewById(R.id.btnupdate);
-        btnupdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myTask = new MyAsyncTask(txttitle,pgbar);
-                myTask.execute(1000);
-            }
-        });
 
-        //取消更新
-        Button btncancel = findViewById(R.id.btncancel);
-        btncancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean flag = myTask.cancel(true);
-                String str;
-                if (flag) {
-                    str = "取消成功";
-                }
-                else {
-                    str = "取消失败";
-                }
-                Toast.makeText(MainActivity.this, str , Toast.LENGTH_LONG).show();
-            }
-        });
-
+        //实例化GestureListener与GestureDetector对象
+        MyGestureListener mgListener = new MyGestureListener();
+        mDetector = new GestureDetector(this, mgListener);
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mDetector.onTouchEvent(event);
+    }
 
+    //自定义一个GestureListener,这个是View类下的，别写错哦！！！
+    private static class MyGestureListener implements GestureDetector.OnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent motionEvent) {
+            Log.d(TAG, "onDown:按下");
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent motionEvent) {
+            Log.d(TAG, "onShowPress:手指按下一段时间,不过还没到长按");
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent motionEvent) {
+            Log.d(TAG, "onSingleTapUp:手指离开屏幕的一瞬间");
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+            Log.d(TAG, "onScroll:在触摸屏上滑动");
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent motionEvent) {
+            Log.d(TAG, "onLongPress:长按并且没有松开");
+        }
+
+        @Override
+        public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+            Log.d(TAG, "onFling:迅速滑动，并松开");
+            return false;
+        }
+    }
 }
