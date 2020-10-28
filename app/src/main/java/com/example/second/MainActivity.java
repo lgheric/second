1f370a0fd7928c09404e931693faee6a232feb21
lgheric <saverneo@163.com>
2020-10-28 16:19:45 +0800

    open new activity by ComponentName of Intent

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.second;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +19,13 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,NewsActivity.class));
+                //1,最常见的启动Activity方式
+                //startActivity(new Intent(MainActivity.this,NewsActivity.class));
+                //通过Intent的ComponentName
+                ComponentName cn = new ComponentName("com.example.second","com.example.second.NewsActivity") ;
+                Intent intent = new Intent() ;
+                intent.setComponent(cn) ;
+                startActivity(intent) ;
             }
         });
 
