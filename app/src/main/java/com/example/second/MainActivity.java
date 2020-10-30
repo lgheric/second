01f7425571d52ec21fe8c982ef9c74aa0284d50f
lgheric <saverneo@163.com>
2020-10-30 22:49:02 +0800

    Verify the calling order of BindService to start the service

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,11 +1,36 @@
 package com.example.second;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.second.service.TestService2;
 
 public class MainActivity extends BaseActivity {
+
+    //保持所启动的Service的IBinder对象,同时定义一个ServiceConnection对象
+    TestService2.MyBinder binder;
+    private ServiceConnection conn = new ServiceConnection() {
+
+        //Activity与Service断开连接时回调该方法
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            System.out.println("------Service DisConnected-------");
+        }
+
+        //Activity与Service连接成功时回调该方法
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            System.out.println("------Service Connected-------");
+            binder = (TestService2.MyBinder) service;
+        }
+    };
 
     @Override
     protected void onDestroy(){
@@ -20,29 +45,36 @@ public class MainActivity extends BaseActivity {
 
         ActivityCollector.addActivity(this);//把当前activity添加到集合中统一管理
 
-
-        Button start = (Button) findViewById(R.id.btnstart);
-        Button stop = (Button) findViewById(R.id.btnstop);
-
-        //创建启动Service的Intent,以及Intent属性
+        Button btnbind = (Button) findViewById(R.id.btnbind);
+        Button btncancel = (Button) findViewById(R.id.btncancel);
+        Button btnstatus = (Button) findViewById(R.id.btnstatus);
         final Intent intent = new Intent();
-        intent.setAction("com.example.second.service.TEST_SERVICE1");
-        intent.setPackage(getPackageName());    //兼容Android 5.0
-
-        //为两个按钮设置点击事件,分别是启动与停止service
-        start.setOnClickListener(new View.OnClickListener() {
+        intent.setAction("com.example.second.service.TEST_SERVICE2");
+        intent.setPackage(getPackageName());
+        btnbind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService(intent);
+                //绑定service
+                bindService(intent, conn, Service.BIND_AUTO_CREATE);
             }
         });
 
-        stop.setOnClickListener(new View.OnClickListener() {
+        btncancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopService(intent);
+                //解除service绑定
+                unbindService(conn);
             }
         });
+
+        btnstatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Service的count的值为:"
+                        + binder.getCount(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
