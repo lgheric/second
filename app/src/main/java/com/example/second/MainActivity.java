4a66ac6fa5f6bf35ae3d9bae6ab6c56745d5ffbc
lgheric <saverneo@163.com>
2020-10-31 22:41:38 +0800

    Optimize the code a little bit.

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,36 +1,9 @@
 package com.example.second;
 
-import android.app.Service;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.example.second.service.TestService2;
 
 public class MainActivity extends BaseActivity {
-
-    //保持所启动的Service的IBinder对象,同时定义一个ServiceConnection对象
-    TestService2.MyBinder binder;
-    private ServiceConnection conn = new ServiceConnection() {
-
-        //Activity与Service断开连接时回调该方法
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            System.out.println("------Service DisConnected-------");
-        }
-
-        //Activity与Service连接成功时回调该方法
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            System.out.println("------Service Connected-------");
-            binder = (TestService2.MyBinder) service;
-        }
-    };
 
     @Override
     protected void onDestroy(){
