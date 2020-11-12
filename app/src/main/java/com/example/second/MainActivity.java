62e0d6bcb1f891e93f2704a33d0abe34a1ea610b
lgheric <saverneo@163.com>
2020-11-12 15:57:02 +0800

    Dynamic registration instance

--- app/src/main/java/com/example/second/MainActivity.java
+++ app/src/main/java/com/example/second/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.second;
 
-import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends BaseActivity {
+
+    MyBRReceiver myReceiver;
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
         ActivityCollector.removeActivity(this);//把当前activity添加到集合中统一管理
+        unregisterReceiver(myReceiver);
     }
 
     @Override
@@ -18,30 +21,11 @@ public class MainActivity extends BaseActivity {
 
         ActivityCollector.addActivity(this);//把当前activity添加到集合中统一管理
 
-        Intent it1 = new Intent("com.example.second.service.TEST_SERVICE3");
-        it1.setPackage(getPackageName());
-        Bundle b1 = new Bundle();
-        b1.putString("param", "s1");
-        it1.putExtras(b1);
-
-        Intent it2 = new Intent("com.example.second.service.TEST_SERVICE3");
-        it2.setPackage(getPackageName());
-        Bundle b2 = new Bundle();
-        b2.putString("param", "s2");
-        it2.putExtras(b2);
-
-        Intent it3 = new Intent("com.example.second.service.TEST_SERVICE3");
-        it3.setPackage(getPackageName());
-        Bundle b3 = new Bundle();
-        b3.putString("param", "s3");
-        it3.putExtras(b3);
-
-        //接着启动多次IntentService,每次启动,都会新建一个工作线程
-        //但始终只有一个IntentService实例
-        startService(it1);
-        startService(it2);
-        startService(it3);
-
+        //核心部分代码：
+        myReceiver = new MyBRReceiver();
+        IntentFilter itFilter = new IntentFilter();
+        itFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(myReceiver, itFilter);
     }
 
 }
