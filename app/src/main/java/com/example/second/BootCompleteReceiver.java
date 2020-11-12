cbbca62df910d53dd0f61efd7472aa8371f55413
lgheric <saverneo@163.com>
2020-11-12 16:13:53 +0800

    The Static registration instance for broadcast receiver

---
+++ app/src/main/java/com/example/second/BootCompleteReceiver.java
@@ -0,0 +1,15 @@
+package com.example.second;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    private final String ACTION_BOOT = "android.intent.action.BOOT_COMPLETED";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (ACTION_BOOT.equals(intent.getAction()))
+            Toast.makeText(context, "开机完毕~", Toast.LENGTH_LONG).show();
+    }
+}
